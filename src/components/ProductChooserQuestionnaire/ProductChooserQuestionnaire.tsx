bd3ccb84bc1cc7b43c57fbe5a86ff45959eee399
Markus Hylleberg <markushylleberg@gmail.com>
2024-02-03 18:07:27 +0100

    feat: adjusted animations + initial content for result card

--- src/components/ProductChooserQuestionnaire/ProductChooserQuestionnaire.tsx
+++ src/components/ProductChooserQuestionnaire/ProductChooserQuestionnaire.tsx
@@ -74,7 +74,7 @@ export default function ProductChooserQuestionnaire({
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0, transition: { delay: 2 } }}
             exit={{ opacity: 0, y: 0, transition: { delay: 0.25 } }}
-            className="pt-20"
+            className="pt-8"
           >
             <QuestionnaireResultCard userInputData={questionnarieData} data={data} />
           </motion.div>
--- src/components/QuestionnaireResultCard/QuestionnaireResultCard.tsx
+++ src/components/QuestionnaireResultCard/QuestionnaireResultCard.tsx
@@ -9,5 +9,16 @@ export default function QuestionnaireResultCard({
   userInputData,
   data,
 }: QuestionnaireResultCardProps) {
-  return <div className="border border-gray-500">results</div>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <span className="font-medium text-[15px]">Vi vil anbefale</span>
+      <h1 className="mt-2 text-[52px] font-light">Clever One</h1>
+      <p className="mt-4 opacity-80 font-normal text-[15px]">
+        Start dagen med fuld energi fra Clever-ladeboksen hjemme, og lad frit på Danmarks største
+        ladenetværk.
+      </p>
+      <div className="mt-10">CTA</div>
+      <button className="mt-8">Kontakt mig</button>
+    </div>
+  )
 }
